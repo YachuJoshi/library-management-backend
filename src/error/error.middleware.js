e857a7bdedf8f567366f10a1cf1531c7be91e940
Yachu Joshi <yachujoshi1234@gmail.com>
2021-07-31 21:39:21 +0545

    Express error handler setup

--- src/error/error.middleware.js
+++ src/error/error.middleware.js
@@ -1,7 +1,7 @@
 import { CustomError } from "./CustomError";
 
-export const errorHandler = (err, _, res) => {
-  console.log(err.message);
+export const errorHandler = (err, req, res, next) => {
+  console.error(err.message);
 
   if (err instanceof CustomError) {
     return res.status(err.code).send(err.message);
