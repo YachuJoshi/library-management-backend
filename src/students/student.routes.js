58d636466a632524f1ee79eee28028179363f520
Yachu Joshi <yachujoshi1234@gmail.com>
2021-08-02 20:28:42 +0545

    Refactored student routes; Include student service

--- src/students/student.routes.js
+++ src/students/student.routes.js
@@ -1,14 +1,71 @@
 import express from "express";
 import {
-  getAllStudents,
-  getStudentById,
-  getStudentBookDetail,
-} from "./student.queries";
+  fetchAllStudents,
+  fetchStudentById,
+  fetchStudentBookDetail,
+} from "./student.servcies";
+import { CustomError } from "../error";
 
 const router = express.Router();
 
-router.get("/", getAllStudents);
-router.get("/:id", getStudentById);
-router.get("/:id/books", getStudentBookDetail);
+router.get("/", async (_, res, next) => {
+  try {
+    const students = await fetchAllStudents();
+    if (students.length > 0) {
+      return res.status(200).json(students);
+    }
+    return next(
+      new CustomError({
+        code: 404,
+        message: "Student Not Found!",
+      })
+    );
+  } catch (e) {
+    return next({});
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const student = await fetchStudentById(id);
+    if (student) {
+      return res.status(200).json(student);
+    }
+    return next(
+      new CustomError({
+        code: 404,
+        message: "Student Not Found!",
+      })
+    );
+  } catch (e) {
+    return next({});
+  }
+});
+
+router.get("/:id/books", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const studentBooksRecord = await fetchStudentBookDetail(id);
+    if (studentBooksRecord.length > 0) {
+      return res.status(200).json(studentBooksRecord);
+    }
+
+    // No student or student has no book
+    const student = await fetchStudentById(id);
+    if (student) {
+      // Student has no books
+      return res.status(200).json([]);
+    }
+    return next(
+      new CustomError({
+        code: 404,
+        message: "Student Not Found!",
+      })
+    );
+  } catch (e) {
+    return next({});
+  }
+});
 
 export default router;
---
+++ src/students/student.servcies.js
@@ -0,0 +1,24 @@
+import { pool } from "../db";
+
+const fetchAllStudents = async () => {
+  const { rows: students } = await pool.query("SELECT * FROM student");
+  return students;
+};
+
+const fetchStudentById = async (id) => {
+  const { rows: student } = await pool.query(
+    "SELECT * FROM student WHERE student_id = $1",
+    [id]
+  );
+  return student[0];
+};
+
+const fetchStudentBookDetail = async (id) => {
+  const { rows: books } = await pool.query(
+    "SELECT * FROM student_book_detail WHERE student_id = $1",
+    [id]
+  );
+  return books;
+};
+
+export { fetchAllStudents, fetchStudentById, fetchStudentBookDetail };
